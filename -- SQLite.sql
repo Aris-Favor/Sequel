41922f6f660fb6e99f14d887386355ae1be5dca4
Aris Favor <aris.s.favor@gmail.com>
2024-01-11 11:02:44 +0300

    New Table

---
+++ -- SQLite.sql
@@ -0,0 +1,11 @@
+-- SQLite
+-- CREATE TABLE People (
+-- Username TEXT NOT NULL PRIMARY KEY,
+-- Password TEXT NOT NULL, 
+-- Email TEXT NOT NULL,
+-- Age INTEGER NOT NULL
+-- );
+
+INSERT INTO People (Username, Password, Email, Age)
+VALUES ('Aris Favor', '#Me210807', 'aris.s.favor@gmail.com', 16);
+
